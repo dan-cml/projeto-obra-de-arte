883bf093d10140b013b313bdaca3ef29187a7ea1
Danilo Marques <dan_cml@hotmail.com>
2023-01-19 19:19:40 -0300

    Criação backend tela de cadastro

--- backend/Controllers/CadastroController.cs
+++ backend/Controllers/CadastroController.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using backend.Context;
+using backend.Models;
 
 namespace backend.Controllers
 {
-    public class CadastroController
+    public class CadastroController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
     }
 }
